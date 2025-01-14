d1acfe27b47d4c4dcfd0fb3d9017467e0301dbd4
aysha zenha <179464573+ayshzenha@users.noreply.github.com>
2025-01-14 14:46:15 +0530

    function completed

--- project-test/script.js
+++ project-test/script.js
@@ -1,1 +1,40 @@
-console.log("This is From js File");
+ const resultElement=document.getElementById("result")
+ let recognition;
+ 
+ 
+ function startConverting(){
+    if('webkitSpeechRecognition 'in window){
+        recognition=new webkitSpeechRecognition
+        setupRecognition(recognition)
+        recognition.start()
+        }
+
+ }
+function setupRecognition(recognition){
+recognition.continuous=true;
+
+recognition.interimResults=true;
+//listening the speech,convert in to the text,display in the screen
+
+recognition.lang="en-US";
+
+recognition.onresult=function(event){
+
+}
+
+
+
+}
+function processResult(results){
+
+}
+
+
+
+
+
+
+
+ function stopConverting(){
+
+ }
