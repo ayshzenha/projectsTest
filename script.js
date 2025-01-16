c19f647f7d5aeb47b6b31c274a4527b0703fa487
aysha zenha <ayshazennah415@gmail.com>
2025-01-16 16:24:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function startConverting() {
 function setupRecognition(recognition) {
     recognition.continuous = true;
     recognition.interimResults = true;
-    recognition.lang = "ar-SA"; // تغيير اللغة إلى العربية
+    recognition.lang = "ar-SA"; // Arabic language
 
     recognition.onresult = function (event) {
         const { finalTranscript, interTranscript } = processResult(event.results);
@@ -51,5 +51,18 @@ function processResult(results) {
 function stopConverting() {
     if (recognition) {
         recognition.stop();
+    }
+}
+
+function copyToClipboard() {
+    const text = resultElement.innerText;
+    if (text) {
+        navigator.clipboard.writeText(text).then(() => {
+            alert('تم نسخ النص إلى الحافظة!');
+        }).catch((err) => {
+            alert('فشل في نسخ النص إلى الحافظة: ' + err);
+        });
+    } else {
+        alert("لا يوجد نص لنسخه!");
     }
 }
